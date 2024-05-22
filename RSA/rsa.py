0d0ef0c6a5ab1c6cdd03432d42a63d5650511771
AinstvonHowling <g.latimer@student.maastrichtuniversity.nl>
2024-05-22 09:30:25 +0200

    minor qol changes

--- RSA/rsa.py
+++ RSA/rsa.py
@@ -4,65 +4,65 @@ from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import hashes
 
 
-class RSA:
-    def __init__(self):
-        pass
+# saving and loading not fully necessary yet, but is useful in irl scenarios and can prevent long key generation times
+def save(private_key, public_key):
+    pem_private_key = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.TraditionalOpenSSL,
+        encryption_algorithm=serialization.NoEncryption()
+    )
+    with open('private_key.pem', 'wb') as f:
+        f.write(pem_private_key)
 
-    def generate_keypair(self):
-        private_key = rsa.generate_private_key(
-            public_exponent=65537,
-            key_size=2048,
+    pem_public_key = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+    with open('public_key.pem', 'wb') as f:
+        f.write(pem_public_key)
+
+
+def generate_keypair():
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+    )
+    public_key = private_key.public_key()
+    return private_key, public_key
+
+
+def load():
+    with open('private_key.pem', 'rb') as f:
+        private_key = serialization.load_pem_private_key(
+            f.read(),
+            password=None
         )
-        public_key = private_key.public_key()
-        return private_key, public_key
-
-    def save_keys_to_files(self, private_key, public_key): # saving and loading not fully necessary yet, but is useful in irl scenarios and can prevent long key generation times
-        pem_private_key = private_key.private_bytes(
-            encoding=serialization.Encoding.PEM,
-            format=serialization.PrivateFormat.TraditionalOpenSSL,
-            encryption_algorithm=serialization.NoEncryption()
+    with open('public_key.pem', 'rb') as f:
+        public_key = serialization.load_pem_public_key(
+            f.read()
         )
-        with open('private_key.pem', 'wb') as f:
-            f.write(pem_private_key)
+    return private_key, public_key
 
-        pem_public_key = public_key.public_bytes(
-            encoding=serialization.Encoding.PEM,
-            format=serialization.PublicFormat.SubjectPublicKeyInfo
+
+def encrypt(public_key, msg):
+    ciphertext = public_key.encrypt(
+        msg,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
         )
-        with open('public_key.pem', 'wb') as f:
-            f.write(pem_public_key)
+    )
+    return ciphertext
 
-    def load_keys_from_files(self):
-        with open('private_key.pem', 'rb') as f:
-            private_key = serialization.load_pem_private_key(
-                f.read(),
-                password=None
-            )
-        with open('public_key.pem', 'rb') as f:
-            public_key = serialization.load_pem_public_key(
-                f.read()
-            )
-        return private_key, public_key
 
-    def encrypt(self, public_key, msg):
-        ciphertext = public_key.encrypt(
-            msg,
-            padding.OAEP(
-                mgf=padding.MGF1(algorithm=hashes.SHA256()),
-                algorithm=hashes.SHA256(),
-                label=None
-            )
+def decrypt(private_key, enc_msg):
+    dec_msg = private_key.decrypt(
+        enc_msg,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
         )
-        return ciphertext
-
-    # Decrypt a message
-    def decrypt(self, private_key, enc_msg):
-        dec_msg = private_key.decrypt(
-            enc_msg,
-            padding.OAEP(
-                mgf=padding.MGF1(algorithm=hashes.SHA256()),
-                algorithm=hashes.SHA256(),
-                label=None
-            )
-        )
-        return dec_msg
+    )
+    return dec_msg
--- RSA/test.py
+++ RSA/test.py
@@ -1,15 +1,18 @@
 from CSPRNG.csprng import CSPRNG
-from RSA.rsa import RSA
+from RSA import rsa
+from pathlib import Path  # good for file checking shenanigans
 
-rsa = RSA()
 
-csprng = CSPRNG(32) # Unused so far, might need generated prime numbers
+csprng = CSPRNG(32)  # Unused so far, might need generated prime numbers
 csprng.rand_int(0, 100)
 
-private_key, public_key = rsa.generate_keypair()
-rsa.save_keys_to_files(private_key, public_key)
-
-private_key, public_key = rsa.load_keys_from_files()
+# print((Path.cwd() / 'private_key.pem').exists(), (Path.cwd() / 'public_key.pem').exists())
+# check if keys have already been created
+if not (Path.cwd() / 'private_key.pem').exists() or not (Path.cwd() / 'public_key.pem').exists():
+    private_key, public_key = rsa.generate_keypair()
+    rsa.save(private_key, public_key)
+else:
+    private_key, public_key = rsa.load()
 
 msg = 'you should commit sewer side'
 msg_byt = msg.encode('utf-8')  # convert to bytes; alternatively add b before str
