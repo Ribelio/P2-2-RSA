aa1c7bd7107e89bce616c00560273b8fadeddec8
Mete Tugay <115257080+metetet@users.noreply.github.com>
2024-05-27 18:15:53 +0200

    experiment stuff

---
+++ RQ1-Experiments/code/chi-square.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from scipy.stats import chisquare
+import numpy as np
+
+path = 'RQ1-Experiments/data/win64_2.csv'
+data = pd.read_csv(path)
+
+# change this to change number of bins 
+bins = np.arange(0, 101, 5) # create 20 bins of 5 numbers (explained why in the report)
+bin_labels = [f"{i}-{i+4}" for i in range(0, 100, 5)]
+alpha = 0.05
+
+# do not change any values beyond this point
+data['Binned'] = pd.cut(data['Value'], bins=bins, labels=bin_labels, right=False)
+observed_frequencies = data['Binned'].value_counts().sort_index()
+
+total_numbers = len(data)
+num_bins = len(bin_labels)
+expected_frequency = total_numbers / num_bins
+
+expected_frequencies = [expected_frequency] * num_bins
+
+chi_square_stat, p_value = chisquare(f_obs=observed_frequencies, f_exp=expected_frequencies)
+
+print(f"Chi-square statistic: {chi_square_stat}")
+print(f"P-value: {p_value}")
+
+if p_value < alpha:
+    print("Reject the null hypothesis: Numbers are not uniformly distributed.")
+else:
+    print("No reason to reject: Numbers seem to be uniformly distributed")
---
+++ RQ1-Experiments/code/runs.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import numpy as np
+
+path = 'RQ1-Experiments/data/win64_2.csv'
+data = pd.read_csv(path)
+
+# parameters that are adjusted
+lower = 0
+upper = 99
+values = 1000
+
+# do not change values beyond this point
+mean_of_uniform_dist = (upper + lower) / 2
+
+above_the_mean = 0
+below_the_mean = 0
+
+for value in data['Value']:
+    if value > mean_of_uniform_dist:
+        above_the_mean += 1
+    elif value < mean_of_uniform_dist:
+        below_the_mean += 1
+
+# runs are defined as: length of sequence of numbers that are above (below) the mean before a value below (above) the mean is generate
+current_run_length = 0
+total_runs = 0
+prev_value = None
+
+for value in data['Value']:
+    if prev_value is None:
+        prev_value = value
+        continue
+    
+    if value > mean_of_uniform_dist and prev_value <= mean_of_uniform_dist:
+        total_runs += 1
+    elif value < mean_of_uniform_dist and prev_value >= mean_of_uniform_dist:
+        total_runs += 1
+    
+    prev_value = value
+
+if prev_value is not None and prev_value != mean_of_uniform_dist:
+    total_runs += 1
+
+print("Total number of runs:", total_runs)
+
+final_mean = ((2 * (above_the_mean) * (below_the_mean)) / values ) + 0.5 # i put 0.5 because that is what we used in SSA, even though online it says 1
+final_variance = (2 * (above_the_mean) * (below_the_mean) * (2 * (above_the_mean) * (below_the_mean) - values)) / ((values * values)*(values - 1))
+
+test_statistic = (total_runs - final_mean) / np.sqrt(final_variance)
+
+print("Final mean:", final_mean)
+print("Final variance", final_variance)
+print("Test statistic:", test_statistic)
+
+z_alpha_half = 1.96
+
+if test_statistic >= (-z_alpha_half) and test_statistic <= z_alpha_half:
+    print("No reason to reject the null hypothesis. The data does not show non-random patterns.")
+else:
+    print("Reject null hypothesis. The data may not be random.")
