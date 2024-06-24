0211aac327f2faf427bd51d25340b86f52cd0122
Ribelio <f.camachoperezdesevilla@student.maastrichtuniversity.nl>
2024-06-24 15:19:12 +0200

    minor spelling mistake

--- RSA/encrypt_CSPRNG.py
+++ RSA/encrypt_CSPRNG.py
@@ -1,5 +1,5 @@
 from csprng import CSPRNG
-import rsa_CSRPNG
+import RSA.rsa_CSPRNG as rsa_CSPRNG
 from pathlib import Path
 
 def encrypt_CSPRNG(message):
@@ -7,18 +7,18 @@ def encrypt_CSPRNG(message):
 
     # generate RSA keys
     #if not (Path.cwd() / 'private_key_temp.pem').exists() or not (Path.cwd() / 'public_key_temp.pem').exists():
-    privatekey, publickey = rsa_CSRPNG.generate_rsa_keypair(csprng)
-    rsa_CSRPNG.save_keys_to_files(privatekey, publickey)
+    privatekey, publickey = rsa_CSPRNG.generate_rsa_keypair(csprng)
+    rsa_CSPRNG.save_keys_to_files(privatekey, publickey)
     # else:
     #     # load keys: this step is just to demonstrate loading. in real usage, keys should already be loaded
     #     privatekey, publickey = rsa_CSRPNG.load_keys_from_files()
 
     # encrypt and decrypt a message
     message1 = message.encode('utf-8')
-    ciphertext1 = rsa_CSRPNG.encrypt_message(publickey, message1)
+    ciphertext1 = rsa_CSPRNG.encrypt_message(publickey, message1)
     print(f'Ciphertext: {ciphertext1}')
 
-    decrypted_message1 = rsa_CSRPNG.decrypt_message(privatekey, ciphertext1)
+    decrypted_message1 = rsa_CSPRNG.decrypt_message(privatekey, ciphertext1)
     print(f'Decrypted message: {decrypted_message1}')
 
     # Write the output to a file
@@ -31,18 +31,18 @@ with open('input.txt', 'r') as file:
 
     # generate RSA keys
     if not (Path.cwd() / 'private_key_temp.pem').exists() or not (Path.cwd() / 'public_key_temp.pem').exists():
-        privatekey, publickey = rsa_CSRPNG.generate_rsa_keypair(csprng)
-        rsa_CSRPNG.save_keys_to_files(privatekey, publickey)
+        privatekey, publickey = rsa_CSPRNG.generate_rsa_keypair(csprng)
+        rsa_CSPRNG.save_keys_to_files(privatekey, publickey)
     else:
         # load keys: this step is just to demonstrate loading. in real usage, keys should already be loaded
-        privatekey, publickey = rsa_CSRPNG.load_keys_from_files()
+        privatekey, publickey = rsa_CSPRNG.load_keys_from_files()
 
     # encrypt and decrypt a message
     message1 = message.encode('utf-8')
-    ciphertext1 = rsa_CSRPNG.encrypt_message(publickey, message1)
+    ciphertext1 = rsa_CSPRNG.encrypt_message(publickey, message1)
     print(f'Ciphertext: {ciphertext1}')
 
-    decrypted_message1 = rsa_CSRPNG.decrypt_message(privatekey, ciphertext1)
+    decrypted_message1 = rsa_CSPRNG.decrypt_message(privatekey, ciphertext1)
     print(f'Decrypted message: {decrypted_message1}')
     
     #Write the output to a file
